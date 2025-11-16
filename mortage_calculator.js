c959eafaa5631b4be2053601f5250a283ea0f689
jamjam8148 <jamjam8148@gmail.com>
2025-11-16 16:49:04 -0500

    Update mortage_calculator.js

--- mortage_calculator.js
+++ mortage_calculator.js
@@ -10,47 +10,47 @@ function mortCalc(myform) {
 	
 document.Form1.Payment.value=monthly(price,interest,numYears)
 document.Form1.Payment.value=punctuation(document.Form1.Payment.value)
-}
+} // The mortCalc function closes here.
 
-
-function monthly(price,interest,numYears) {
-    // Formula for Monthly Payment (P * r * (1+r)^n) / ((1+r)^n - 1)
-    // where P=Price, r=Monthly Rate (Interest/1200), n=Number of Payments (Years*12)
-	var IntRate=interest/1200
-	var Pmts=numYears*12
-	var pay=price
-	return pay*(IntRate/(1-(1/Math.pow(1+IntRate,Pmts))))
-}
-
-function punctuation(valuein) {
-    // Function to format the output with dollar sign and commas
-	var formatStr=""
-	var Odollars=""
-	var decpos=valuein.indexOf(".")
-	if (decpos==-1)
-		decpos=valuein.length
-	var dollars=valuein.substring(0,decpos)
-	var Fdollars=dollars.length
-	if (Fdollars>3) {
-		while (Fdollars>0) {
-		Tdollars=dollars.substring(Fdollars-3,Fdollars)
-		if (Tdollars.length==3) {
-			Odollars=","+Tdollars+Odollars
-			Fdollars=Fdollars-3
-		} else {
-			Odollars=Tdollars+Odollars
-			Fdollars=0
-			}
-		}
-	if (Odollars.substring(0,1)==",")
-		dollars=Odollars.substring(1,Odollars.length)
-	else
-		dollars=Odollars
+	
+	function monthly(price,interest,numYears) {
+	    // Formula for Monthly Payment (P * r * (1+r)^n) / ((1+r)^n - 1)
+	    // where P=Price, r=Monthly Rate (Interest/1200), n=Number of Payments (Years*12)
+		var IntRate=interest/1200
+		var Pmts=numYears*12
+		var pay=price
+		return pay*(IntRate/(1-(1/Math.pow(1+IntRate,Pmts))))
 	}
-	var cents=valuein.substring(decpos+1,decpos+3)
-	if (cents=="")
-		cents="00"
-	var formatStr="$"+dollars+"."+cents
 
-	return formatStr
-}
+	function punctuation(valuein) {
+	    // Function to format the output with dollar sign and commas
+		var formatStr=""
+		var Odollars=""
+		var decpos=valuein.indexOf(".")
+		if (decpos==-1)
+			decpos=valuein.length
+		var dollars=valuein.substring(0,decpos)
+		var Fdollars=dollars.length
+		if (Fdollars>3) {
+			while (Fdollars>0) {
+			Tdollars=dollars.substring(Fdollars-3,Fdollars)
+			if (Tdollars.length==3) {
+				Odollars=","+Tdollars+Odollars
+				Fdollars=Fdollars-3
+			} else {
+				Odollars=Tdollars+Odollars
+				Fdollars=0
+				}
+			}
+		if (Odollars.substring(0,1)==",")
+			dollars=Odollars.substring(1,Odollars.length)
+		else
+			dollars=Odollars
+		}
+		var cents=valuein.substring(decpos+1,decpos+3)
+		if (cents=="")
+			cents="00"
+		var formatStr="$"+dollars+"."+cents
+	
+		return formatStr
+	} // The punctuation function closes here.
